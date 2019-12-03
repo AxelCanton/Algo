e66a32eba7f4f1c312cab810acfd9b6f23f4b416
Alexiaognard <43749049+Alexiaognard@users.noreply.github.com>
2019-12-03 16:40:34 +0100

    Add files via upload
    correction2

--- typeAbstrait/plateauProtocol.swift
+++ typeAbstrait/plateauProtocol.swift
@@ -9,8 +9,7 @@ import Foundation
 protocol plateauProtocol{
     //init : -> plateauProtocol
     //fonction qui créée un plateau vide
-    //données : rien
-    //résultat : un plateau vide contenant 4x4 cases vides
+    //résultat : une grille contenant 16 cases vides ayant des coordonnées x,y comprises entre [0,4]
     init()
     
     // ajouterPiece : plateauProtocol x caseProtocol x pieceProtocol x joueurProtocol -> caseProtocol
@@ -18,6 +17,13 @@ protocol plateauProtocol{
     //données : une case, une pièce et un joueur
     //résultat : renvoie la case avec la pièce du joueur ajoutée
     //post : si la case est déjà occupée renvoie une erreur, si le joueur a déjà utilisé ses 2 pièces de la même forme renvoie une erreur et s'il existe une pièce de même forme de l'autre joueur dans la même colonne que la case ou la même ligne que la case ou la même région que la case renvoie une erreur
-    //post: estvide(ajouterPiece(piece,case,joueur)=false
-    func ajouterPiece(case : caseProtocol, piece:pieceProtocol, joueur:joueurProtocol) throws ->caseProtocol
+    //post: estvide(ajouterPiece(piece,case,joueur)=false, et nbxxxRestant se décrémente de 1 (avec xxx correspondant au nom de la pièce)
+    mutating func ajouterPiece(case : caseProtocol, piece:pieceProtocol, joueur:joueurProtocol) throws -> plateauProtocol
+    
+    //getCase : plateauProtocol x Int x Int -> caseProtocol
+    //fonction qui renvoie la case correspondante à la position donnée
+    //données : x et y, 2 entiers, correspondant à la position de la case
+    //résultat : renvoie la case correspondante à la position
+    //post : postion(getCase(x:Int,y:Int))=(x,y)
+    func getCase(x:Int,y:Int)->caseProtocol
 }
