ddfeb576135b996bbd33f3aa522b733a65265f31
Alexiaognard <43749049+Alexiaognard@users.noreply.github.com>
2019-12-07 19:48:15 +0100

    Update caseProtocol.swift

--- typeAbstrait/caseProtocol.swift
+++ typeAbstrait/caseProtocol.swift
@@ -14,7 +14,7 @@ protocol caseProtocol{
     //données : 2 entiers x et y où x représente la ligne et y la colonne où se trouve la case
     //pré : x et y sont compris entre 0 et 3 ( la position (0,0) représente la case tout en haut à gauche et (3,3) la case tout en bas à droite)
     //résultat : renvoie une case vide
-    //post : estVide = true, piece=vide, joueur=vide
+    //post : init(x:x,y:y).estVide = true, init(x:x,y:y).piece=vide, init(x:x,y:y).joueur=vide, init(x:x,y:y).position=(x,y)
     init(x:Int,y:Int)
     
     var estVide : Bool {get set} // une case est vide s'il n'ya pas de pièce posée par un joueur
